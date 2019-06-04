29fd43c70c16412c0dec8ee8e435a4ba29c1310e
Mark Wight <31548252+mbwight@users.noreply.github.com>
2019-06-03 18:42:35 -0700

    Update index.js

--- ResumeInReact/index.js
+++ ResumeInReact/index.js
@@ -1,12 +1,4 @@
-// Objectives:
-// 1. Set up the basic React code from scratch
-// 2. Create a functional component called MyInfo that returns the following UI:
-    // a. An h1 with your name
-    // b. A paragraph with a little blurb about yourself
-    // c. An ordered or unordered list of the top 3 vacation spots you'd like to visit
-// 3. Render an instance of that functional component to the browser
-// Extra challenge: learn on your own (Google!) how you can add some style to your page.
-// (We will also cover this in an upcoming lesson).
+
 
 import React from "react"
 import REACTDOM from "react-dom"
